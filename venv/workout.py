5e499e473dec64cbeb71dc7eb38f67a926e9c7dd
gnganapath <gnganapathi@gmail.com>
2021-05-20 19:50:27 +0530

    def for contructor

---
+++ venv/workout.py
@@ -0,0 +1,28 @@
+# this  is commands for about  the file and purpose of code  -- Single Line commands
+...
+# block command
+# again next line
+# block comment
+...
+
+
+class student:
+    def __init__():  # this is called python constructor
+        pass
+
+    def get_student(self):
+        pass
+
+    def add_student(self):
+        pass
+
+    def delete_student(self):
+        pass
+
+    def total_and_average(self):
+        # Depends on the total number of the subject
+
+        #total+
+
+        #avg of student calc
+        #avg/5
