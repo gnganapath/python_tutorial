86f616ffe009d650dedb02b99a1ebc1a0dc022b7
gnganapath <gnganapathi@gmail.com>
2022-01-14 20:05:57 +0530

    foir loop

---
+++ UdemyAvinash/forloop.py
@@ -0,0 +1,15 @@
+list1 = ["apples", "banana", "cheery"]
+tup1 = (13, 12, 15)
+for item in list1:
+    print(item)
+
+apples
+banana
+cheery
+
+
+for item in tup1:
+    print(item)
+    13
+    12
+    15
